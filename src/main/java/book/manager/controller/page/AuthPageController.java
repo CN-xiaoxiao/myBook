d8734d73cc1c697391d59ce3163ad4959b8cccab
CN-xiaoxiao <17375603959@163.com>
2022-06-09 21:41:49 +0800

    first commit

---
+++ src/main/java/book/manager/controller/page/AuthPageController.java
@@ -0,0 +1,21 @@
+package book.manager.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xiaoxiao
+ */
+@Controller
+@RequestMapping("/page/auth")
+public class AuthPageController {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
